23e835ecb1517710a91f5df59600c70af369267a
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 07:32:45 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,14 @@ def reverse_each_word(sentence)
   nu_sentence = sentence.split(" ")
   nu_sentence.each do |word|
     word.reverse!
+    nu_sentence = "#{nu_sentence} #{word}"
   end
-  nu_sentence.join(" ")
 end
 
 def reverse_each_word(sentence)
   nu_sentence = sentence.split(" ")
-  nu_sentence.collect do |word|
+  nu_sentence.map do |word|
     word.reverse!
+    nu_sentence = "#{nu_sentence} #{word}"
   end
-  nu_sentence.join(" ")
 end
